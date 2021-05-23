194460eb83d61c96598f5caae7627a54ce2b0706
Catalin-Gabriel Radu <catalin.radu@anais.digital>
2021-05-23 16:07:10 +0300

    changes 23 may

--- model.py
+++ model.py
@@ -8,17 +8,18 @@ class GCN(nn.Module):
 	def __init__(self, input_size, hidden_size, output_size):
 		super(GCN, self).__init__()
 
-		self.linear1 = nn.Linear(input_size, hidden_size)
+		self.conv1 = GraphConvolutionLayer(input_size, hidden_size)
+		self.conv2 = GraphConvolutionLayer(hidden_size, 300)
+
+		self.linear1 = nn.Linear(300, hidden_size)
 		self.linear2 = nn.Linear(hidden_size, 50)
-		self.linear3 = nn.Linear(50, 200)
-		self.conv1 = GraphConvolutionLayer(200, 500)
-		self.conv2 = GraphConvolutionLayer(500, output_size)
+		self.linear3 = nn.Linear(50, output_size)
 
 	def forward(self, x, A):
+		x = F.relu(self.conv1(x, A))
+		x = F.relu(self.conv2(x, A))
 		x = F.relu(self.linear1(x))
 		x = F.relu(self.linear2(x))
 		x = self.linear3(x)
-		x = self.conv1(x, A)
-		x = F.relu(self.conv2(x, A))
 		# x = torch.sigmoid(x)
 		return x
